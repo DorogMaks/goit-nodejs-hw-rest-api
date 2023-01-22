c6347dec6ec1094d351d21031d427d97639ad06c
Maksym Dorohokuplya <dorogmaks@gmail.com>
2023-01-22 16:40:36 +0200

    add fn validateToken

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,9 @@
 const validateBody = require('./validateBody');
 const validateId = require('./validateId');
+const validateToken = require('./validateToken');
 
 module.exports = {
   validateBody,
   validateId,
+  validateToken,
 };

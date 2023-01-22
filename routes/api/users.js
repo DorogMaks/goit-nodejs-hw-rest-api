82815aaeb2eecb560c53689f263be0222a8ebc9f
Maksym Dorohokuplya <dorogmaks@gmail.com>
2023-01-22 17:53:23 +0200

    add current

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { validateBody, validateToken } = require('../../middleware');
 const { authUserSchema } = require('../../schemas/joi/users');
 const { asyncWrapper } = require('../../helpers');
-const { signup, login, logout } = require('../../controllers/users');
+const { signup, login, logout, current } = require('../../controllers/users');
 
 const usersRouter = express.Router();
 
 usersRouter.post('/signup', validateBody(authUserSchema), asyncWrapper(signup));
 usersRouter.post('/login', validateBody(authUserSchema), asyncWrapper(login));
 usersRouter.post('/logout', validateToken(), asyncWrapper(logout));
+usersRouter.post('/current', validateToken(), asyncWrapper(current));
 
 module.exports = usersRouter;

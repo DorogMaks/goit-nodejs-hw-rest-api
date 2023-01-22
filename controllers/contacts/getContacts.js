d569b02e2f199485a1826e474ce353b25325e1fa
Maksym Dorohokuplya <dorogmaks@gmail.com>
2023-01-22 22:22:19 +0200

    add filtering by favorite

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -2,7 +2,24 @@ const { Contact } = require('../../models/contact');
 const { HttpError } = require('../../helpers');
 
 const getContacts = async (req, res, next) => {
-  const contacts = await Contact.find({});
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+
+  console.log(favorite);
+
+  if (favorite !== 'true') {
+    const contacts = await Contact.find({}).skip(skip).limit(limit);
+
+    if (!contacts.length) {
+      return next(new HttpError(404, 'Contacts not found'));
+    }
+
+    return res.json(contacts);
+  }
+
+  const contacts = await Contact.find({ favorite: favorite })
+    .skip(skip)
+    .limit(limit);
 
   if (!contacts.length) {
     return next(new HttpError(404, 'Contacts not found'));
